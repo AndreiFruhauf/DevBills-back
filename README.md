acfe2efe9763008d4110eb9b8886deddeee3471f
Dinoh665 <fruhaufandrei@gmail.com>
2024-10-01 10:36:54 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Listar as transações com filtros (DONE)
+    - por nome
+    - por período (início e fim)
+    - por categoria
+
+# Listar as categorias (DONE)
+
+# Dados de saldo com filtros (DONE)
+    - por período (início e fim)
+
+# Dados de despesas por categoria com filtros (DONE)
+    - por período
+
+# Dados de balanço com filtros (DONE)
+    - por ano
+
+# Criar categorias (DONE)
+    - nome, cor
+    - Não é possível criar categorias repetidas
+
+# Criar transações (DONE)
+    - categoria, nome, valor, data, tipo
---
+++ src/routes/transactions.routes.ts
@@ -0,0 +1,38 @@
+import { Router } from "express";
+import { ParamsType, validator } from "../middlewares/validator.middleware";
+import { createTransactionsSchema, getDashboardSchema, getFinancialEvolutionSchema, indexTransactionsSchema } from "../dtos/transactions.dto";
+import { TransactionsFactory } from "../factories/transactions.factory";
+import { TransactionController } from "../controllers/transaction.controller";
+
+
+export const transactionsRoutes = Router()
+
+const controller = new TransactionController(TransactionsFactory.getServiceInstance())
+
+transactionsRoutes.post('/', validator({
+    schema: createTransactionsSchema,
+    type: ParamsType.BODY
+}), controller.create)
+
+
+transactionsRoutes.post('/', validator({
+    schema: createTransactionsSchema,
+    type: ParamsType.BODY
+}), controller.create)
+
+transactionsRoutes.get('/', validator({
+    schema: indexTransactionsSchema,
+    type: ParamsType.QUERY
+}), controller.index)
+
+transactionsRoutes.get('/dashboard', validator({
+    schema: getDashboardSchema,
+    type: ParamsType.QUERY
+}), controller.getDashboard)
+
+
+transactionsRoutes.get('/financial-evolution', validator({
+    schema: getFinancialEvolutionSchema,
+    type: ParamsType.QUERY
+}), controller.getFinancialEvolution)
+
